1494d1adb089b23fe0709f06f905d5b229db5b6c
Devesh Chandra <deveshru2712@gmail.com>
2025-09-10 16:09:05 +0530

    installed the better auth with the schema

---
+++ app/lib/auth.ts
@@ -0,0 +1,15 @@
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { prisma } from "./prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  socialProviders: {
+    github: {
+      clientId: process.env.AUTH_GITHUB_ID as string,
+      clientSecret: process.env.AUTH_GITHUB_SECRET as string,
+    },
+  },
+});
